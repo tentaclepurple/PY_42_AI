9789b31f382594fd160f8bd555199f6a15d6373e
tentaclepurple <116112114+tentaclepurple@users.noreply.github.com>
2023-07-21 12:31:24 +0200

    Add files via upload

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -30,8 +30,48 @@ def print_rec_and_details(rec, dic):
 				else:
 					print(f"{key}: {value}")
 
+def del_recipe(rec, dic):
+    deleted = dic.pop(rec, None)
+    if deleted == None:
+        print("Recipe is not in Cookbook")
+        
+def add_recipe(dic):
+    new_recipe = input("Enter a name for a new recipe: ")
+    list_ingreds = []
+    print("Enter one ingredient: ")
+    while True:
+        new_ingred = input("")
+        if not new_ingred:
+            break
+        list_ingreds.append(new_ingred)
+    meal_type = input("Enter a meal type: ")
+    time = int(input("Enter a preparation time: "))
+    new_inp = {
+	'ingredients' : list_ingreds,
+	'meal' : meal_type,
+	'prep_time' : time
+	}
+    dic[new_recipe] = new_inp
 
+while True:
 
-print_recipe_names(cookbook)
-print()
-print_rec_and_details('Salad', cookbook)
+    try:
+        option = int(input("Enter an option: \n	1: Add a recipe\n	2: Delete a recipe\n	3: Print a recipe\n	4: Print the cookbook\n	5: Quit\n"))
+
+        if option == 1:
+            add_recipe(cookbook)
+        elif option == 2:
+            recipe = input("Enter a recipe name to get its details: ")
+            print_rec_and_details(recipe, cookbook)
+        elif option == 3:
+            recipe = input("Enter a recipe name to get its details: ")
+            print_rec_and_details(recipe, cookbook)
+        elif option == 4:
+            print(cookbook)
+        elif option == 5:
+            break
+        else:
+            print("Sorry, this option does not exist.")
+    except ValueError:
+        print("Invalid input. Try again")
+
