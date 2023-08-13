b7b6fee8c8e9ace2234caac5d91a64cc4f079e4a
tentaclepurple <116112114+tentaclepurple@users.noreply.github.com>
2023-08-14 01:28:57 +0200

    Delete ex05 directory

--- ex05/kata00.py
+++
@@ -1,4 +0,0 @@
-kata = (19,42,21)
-def prtuple(tup):
-    print(f"The 3 numbers are: {tup[0]}, {tup[1]}, {tup[2]}")
-prtuple(kata)
--- ex05/kata02.py
+++
@@ -1,8 +0,0 @@
-# Put this at the top of your kata02.py file
-kata = (2019, 9, 25, 3, 30)
-
-def adjust(a):
-	if a < 10:
-		return ('0' + str(a))
-
-print(f"{adjust(kata[1])}/{kata[2]}/{kata[0]} {adjust(kata[3])}:{kata[4]}")
